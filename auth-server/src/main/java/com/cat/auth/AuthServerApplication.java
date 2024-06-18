8e0801b378b106dfbd60cd627b7fec30da200d4a
小猫会发光 <xiaomaohuifaguang@163.com>
2024-06-18 22:26:39 +0800

    :tada: 初始化

---
+++ auth-server/src/main/java/com/cat/auth/AuthServerApplication.java
@@ -0,0 +1,20 @@
+package com.cat.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/***
+ * 鉴权服务主启动类
+ * @title AuthServerApplication
+ * @description <TODO description class purpose>
+ * @author xiaomaohuifaguang
+ * @create 2024/6/18 22:20
+ **/
+@SpringBootApplication
+public class AuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerApplication.class,args);
+    }
+
+}
