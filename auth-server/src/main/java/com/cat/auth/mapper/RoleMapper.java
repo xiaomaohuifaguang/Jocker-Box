23b827c185efdebc6b129e13ad5dbb0d37b759a2
小猫会发光 <xiaomaohuifaguang@163.com>
2024-06-24 00:59:36 +0800

    :sparkles: 引入mybatis-plus

---
+++ auth-server/src/main/java/com/cat/auth/mapper/RoleMapper.java
@@ -0,0 +1,17 @@
+package com.cat.auth.mapper;
+
+import com.cat.common.entity.Role;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+/**
+ * <p>
+ * 角色表 Mapper 接口
+ * </p>
+ *
+ * @author xiaomaohuifaguang
+ * @since 2024-06-23
+ */
+@Mapper
+public interface RoleMapper extends BaseMapper<Role> {
+
+}
